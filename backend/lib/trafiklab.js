ddc3748a0e0268cf335257af665e0d20d8a1c5a7
grayoverride <johan.b.damm@gmail.com>
2018-11-11 10:01:51 +0100

    lets stop commiting api keys

--- backend/lib/trafiklab.js
+++ backend/lib/trafiklab.js
@@ -1,6 +1,6 @@
 const axios = require("axios");
 const travelTime = require('./travelTime');
-
+const secrets = require('../secrets');
 const getData = async url => {
     try {
         const response = await axios.get(url);
@@ -13,14 +13,14 @@ const getData = async url => {
 const endpoints = {
     search: async (query) => {
         try {
-            return await getData(`https://api.sl.se/api2/typeahead.json?key=2a17ce62b7af40609eebae8c590ad38f&searchstring=${query}`)
+            return await getData(`https://api.sl.se/api2/typeahead.json?key=${secrets.platsuppslag}&searchstring=${query}`)
         } catch (error) {
             return error
         }
     },
     calculateTravel: async (from, to) => {
         try {
-            const data = await getData(`https://api.sl.se/api2/TravelplannerV3/trip.json?key=3cef19e7a6284292b3b1e154e7278cb9&lang=en&originId=${from}&destId=${to}`)
+            const data = await getData(`https://api.sl.se/api2/TravelplannerV3/trip.json?key=${secrets.reseplanerare}&lang=en&originId=${from}&destId=${to}`)
             return travelTime.estimate(data.Trip)
         } catch (error) {
             return error
